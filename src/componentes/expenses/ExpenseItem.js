c3d8c0b2ee607715b81a0563cc0b36062968eab8
Amanda Souza <amandasrs1001@gmail.com>
2023-01-05 13:37:18 -0300

    Created ExpenseList and conditional rendered

--- src/componentes/expenses/ExpenseItem.js
+++ src/componentes/expenses/ExpenseItem.js
@@ -1,32 +1,26 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ExpenseCard from '../UI/ExpenseCard.js'
 import './ExpenseDate.js'
 import ExpenseDate from './ExpenseDate.js'
 import './ExpenseItem.css'
 
 function ExpenseItem (props) {
-     const [DefaultTitle, SetTitle] = useState(props.title)
-
-    function ClickHandler () {
-        SetTitle ('Updated')
-    }
 
     return (
+        <li>
         <ExpenseCard className="expense-item">
             <ExpenseDate
             date = {props.date}></ExpenseDate>
             <div className='expense-item__description'>
                 <h2>
-                    {DefaultTitle}
+                    {props.title}
                 </h2>
             </div>
             <div className="expense-item__price">
                 ${props.amount}
             </div>
-            <button onClick={ClickHandler}>
-                Change Title
-            </button>
         </ExpenseCard>
+        </li>
     )
 }
 
--- src/componentes/expenses/Expenses.js
+++ src/componentes/expenses/Expenses.js
@@ -1,47 +1,35 @@
 import React, {useState} from 'react';
 import ExpensesFilter from "../filter/ExpenseFilter.js"
 import ExpenseCard from "../UI/ExpenseCard.js"
-import ExpenseItem from "./ExpenseItem.js"
+import ExpenseList from './ExpenseList.js';
 import './Expenses.css'
 
 
 function Expenses (props) {
+  
+const [inputFilter, SetInputFilter] = useState('2020')
 
-  const [inputFilter, SetInputFilter] = useState('2020')
 
-  const InputFilterHandle = enteredInputFilter => {
-    SetInputFilter(enteredInputFilter)
-    console.log(enteredInputFilter);
-  }
+const InputFilterHandle = (enteredInputFilter) => {
+  SetInputFilter(enteredInputFilter)
+}      
 
-  return (
+const FilteredExpenses = props.itens.filter(expense => {
+  if (expense.date.getFullYear() == inputFilter) {
+        return true
+      }
+    })
+
+return (
+
   <ExpenseCard className="Expenses">
 
-  <ExpensesFilter select={inputFilter} onInputFilter={InputFilterHandle}></ExpensesFilter>
+  <ExpensesFilter 
+  select={inputFilter} 
+  onInputFilter={InputFilterHandle}></ExpensesFilter>
 
-  <ExpenseItem 
-  title = {props.itens[0].title}
-  amount = {props.itens[0].amount}
-  date = {props.itens[0].date}
-  ></ExpenseItem>
+ <ExpenseList items = {FilteredExpenses}></ExpenseList>
 
-  <ExpenseItem 
-  title = {props.itens[1].title}
-  amount = {props.itens[1].amount}
-  date = {props.itens[1].date}
-  ></ExpenseItem>
-
-  <ExpenseItem 
-  title = {props.itens[2].title}
-  amount = {props.itens[2].amount}
-  date = {props.itens[2].date}
-  ></ExpenseItem>
-
-  <ExpenseItem 
-  title = {props.itens[3].title}
-  amount = {props.itens[3].amount}
-  date = {props.itens[3].date}
-  ></ExpenseItem> 
   </ExpenseCard>
   )
 }
