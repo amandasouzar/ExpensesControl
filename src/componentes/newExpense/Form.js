40f6e33f30718c00ba2e85e8dd498a1eb984ca13
Amanda Souza <amandasrs1001@gmail.com>
2023-01-05 15:57:33 -0300

    Set Close Form to Cancel and Add Button

--- src/componentes/newExpense/Form.js
+++ src/componentes/newExpense/Form.js
@@ -34,29 +34,31 @@ function Form(props) {
         SetNoDate('')
     }
 
-    return (
-        <form onSubmit={SubmitHandler}>
-            <div className='new-expense__controls'>
-                <div className='new-expense__control'>
-                    <label>Title</label>
-                    <input type="text" value={NoTitle} onChange={titleChangeHandler}></input>
-                </div>
-                <div className='new-expense__control'>
-                    <label>Amount</label>
-                    <input type="number" value={NoAmount} onChange={amountChangeHandler} min="0.01" step="0.01"></input>
-                </div>
-                <div className='new-expense__control'>
-                    <label>Date</label>
-                    <input type="date" value={NoDate} onChange={dateChangeHandler} min='2019-01-01' max="2023-12-31"></input>
-                </div>
-            </div>
-            <div className='new-expense__actions'>
-                <button type='submit'>
-                    Add Expense
-                </button>
-            </div>
-        </form>
-    )
+    return <form onSubmit={SubmitHandler}>
+    <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+            <label>Title</label>
+            <input type="text" value={NoTitle} onChange={titleChangeHandler}></input>
+        </div>
+        <div className='new-expense__control'>
+            <label>Amount</label>
+            <input type="number" value={NoAmount} onChange={amountChangeHandler} min="0.01" step="0.01"></input>
+        </div>
+        <div className='new-expense__control'>
+            <label>Date</label>
+            <input type="date" value={NoDate} onChange={dateChangeHandler} min='2019-01-01' max="2022-12-31"></input>
+        </div>
+    </div>
+    <div className='new-expense__actions'>
+        <button type='button' onClick={props.ClickHandler}>
+            Cancel
+        </button>
+        <button type='submit'>
+            Add Expense
+        </button>
+    </div>
+    </form> 
+
 }
 
 export default Form
--- src/componentes/newExpense/NewExpense.js
+++ src/componentes/newExpense/NewExpense.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react'
 import './NewExpense.css'
 import Form from './Form.js'
 
@@ -8,12 +9,24 @@ function NewExpense (props) {
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData)
+        SetShowForm(false)
+    }
+
+    const [ShowForm, SetShowForm] = useState(false)
+
+    const ShowFormHandler = () => {
+        SetShowForm(true)
+    }
+
+    const OnClickHandler = () => {
+        SetShowForm(false)
     }
 
     return (
         <div className='new-expense'>
-            <Form onSaveExpenseData={SaveExpenseDataHandler}
-            ></Form>
+            {!ShowForm && <button onClick={ShowFormHandler}>Add New Expense</button>}}
+
+            {ShowForm &&<Form ClickHandler= {OnClickHandler} onSaveExpenseData={SaveExpenseDataHandler}></Form>}
         </div>
     )
 }
